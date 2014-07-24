7ff7f0290c31e74fff4ea791ddcc7ff03e22dc0f
stanley <flywww004@gmail.com>
2014-07-24 19:22:56 +0800

    Initial Commit

---
+++ Caloshop/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Caloshop
+//
+//  Created by 林盈志 on 7/24/14.
+//  Copyright (c) 2014 林盈志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Caloshop/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Caloshop
+//
+//  Created by 林盈志 on 7/24/14.
+//  Copyright (c) 2014 林盈志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
